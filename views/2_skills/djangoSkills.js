9874b2eb66b2c862e206fdfa4d7d26dd010c907b
zombie1864 <zombie1864@gmail.com>
2021-10-19 11:24:55 -0400

    updated img src

--- views/2_skills/djangoSkills.js
+++ views/2_skills/djangoSkills.js
@@ -2,7 +2,7 @@ document.write(`
     <div class="col-sm-12 col-md-6 col-lg-12 col-xl-6 mb-4">
         <div class="media tm-bg-transparent-black tm-border-white" id="SQL">
             <i class="tm-icon-circled tm-icon-media">
-                <img src="img/django.jpeg" width=80 height =80>
+                <img src="img/django.jpeg" width=80 height=80 style="border-radius:50%">
             </i>
             <div class="media-body">
                 <h3>Django</h3>
--- views/2_skills/flaskSkills.js
+++ views/2_skills/flaskSkills.js
@@ -2,7 +2,7 @@ document.write(`
 <div class="col-sm-12 col-md-6 col-lg-12 col-xl-6 mb-4"> 
 <div class="media tm-bg-transparent-black tm-border-white" id="flask">
 <i class="tm-icon-circled tm-icon-media">
-    <img src="img/flask.jpg" width=85 height =85 style="border-radius:50%">
+    <img src="img/flask.jpg" width=80 height=80 style="border-radius:50%">
 </i>
     <div class="media-body">
         <h3>Flask</h3>
