e4343e51fcec85dc127ee92e2662714a1565e675
zombie1864 <zombie1864@gmail.com>
2021-10-19 11:12:48 -0400

    change path of img src for skills page p5js

--- views/2_skills/p5jsSkills.js
+++ views/2_skills/p5jsSkills.js
@@ -2,7 +2,7 @@ document.write(`
 <div class="col-sm-12 col-md-6 col-lg-12 col-xl-6 mb-4">
 <div class="media tm-bg-transparent-black tm-border-white" id="p5">
     <i class="tm-icon-circled tm-icon-media">
-        <img src="/Users/jeff/Desktop/App_Academy/zombie1864.github.io/img/p5.png" width=80 height =80 style="border-radius:50%">
+        <img src="img/p5.png" width=80 height =80 style="border-radius:50%">
     </i>	
     <div class="media-body">
         <h3>p5.JS</h3>
